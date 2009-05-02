dba05077a32d4427d54ac2c7b2c0fe34e7a83331
Jürgen Geuter <tante@the-gay-bar.com>
2009-05-02 16:54:19 +0200

    Added more configuration options and time parsing

--- gmaillabelpurge.py
+++ gmaillabelpurge.py
@@ -8,6 +8,9 @@
 
 import imaplib
 import os.path
+import email.utils
+import datetime
+import time
 
 CONFIGFILE="~/.gmaillabelpurge"
 """The filename and path of the config file, default is ~/.gmailpurge"""
@@ -16,10 +19,23 @@ _config=None
 """_config contains the configuration as parsed from CONFIGFILE"""
 
 def readConf():
+    """Read the configuration file and die with a helpful error message
+    when the file isn't valid for some reason."""
     global _config
     _config={}
-    handle = open(os.path.expanduser(CONFIGFILE),"r")
-    data = handle.readlines()
+    try:
+        handle = open(os.path.expanduser(CONFIGFILE),"r")
+        data = handle.readlines()
+    except:
+        raise SystemExit("""Please set up the configuration in %s
+Example:
+
+username=myuser@gmail.com
+password=MYPASSWORD
+maxage=30
+labels=LABEL1,LABEL2
+        
+""" % os.path.expanduser(CONFIGFILE))
     if len(data)<2:
         raise SystemExit("Please setup you configuration in %s!" % os.path.expanduser(CONFIGFILE))
     for line in data:
@@ -28,6 +44,14 @@ def readConf():
             _config['username']=value.strip()
         if param.strip().lower()=="password":
             _config['password']=value.strip()
+        if param.strip().lower()=="maxage":
+            _config['maxage']=int(value.strip())
+        if param.strip().lower()=="labels":
+            labels=[]
+            for label in value.split(","):
+                labels.append(label.strip())
+            _config['labels']=labels
+
     try:
         _config['username']
     except:
@@ -36,18 +60,55 @@ def readConf():
         _config['password']
     except:
         raise SystemExit("Please set a password in your config file")
+    try:
+        _config['maxage']
+    except:
+        raise SystemExit("Please set the maximum age of mails to keep (via 'maxage')")
+    try:
+        _config['labels']
+    except:
+        raise SystemExit("Please set labels to be purged")
 
-
-def connect():
+def purge(verbose=False):
     global _config
-    M=imaplib.IMAP4_SSL("imap.gmail.com", 993)
-    M.login(_config['username'],_config['password'])
-    status, count = M.select("Inbox")
-    status, data = M.fetch(count[0], "(UID BODY[TEXT])")
+    server=imaplib.IMAP4_SSL("imap.gmail.com", 993)
+    try:
+        server.login(_config['username'],_config['password'])
+    except:
+        raise SystemExit("Couldn't connect to Gmail server, is the name/password combination correct?")
+    try:
+        status, count = server.select("testlabel")
+    except:
+        raise SystemExit("The given label doesn't seem to exist.")
+    
+    # mark the current date so we can compare the mail ages
+    now = datetime.datetime.now()
+    if verbose:
+        print("Current time: %s" % now.isoformat())
 
-    print data[0][1]
-    M.close()
-    M.logout()
+    status, data = server.fetch(count[0], "(UID BODY[HEADER.FIELDS (SUBJECT FROM DATE)])")
+    headers={}
+    for header in data[0][1].split("\n"):
+        try:
+            name,value = header.strip().split(":",1)
+            headers[name.strip().lower()] = value.strip()
+        except:
+            # we just don't care what went wrong here
+            pass
+    #copy the mail to the trash
+   # server.copy(count[0],"[Google Mail]/Trash") 
+
+    datetuple = list(email.utils.parsedate(headers['date']))
+    
+    # delete timezoneoffsets, might have to deal with that later
+    del datetuple[7]
+    del datetuple[7]
+    maildate = datetime.datetime(*datetuple)
+    delta=now-maildate
+    
+    # close the connection
+    server.close()
+    server.logout()
 
 readConf()
-connect()
+purge(True)
