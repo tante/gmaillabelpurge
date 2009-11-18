f5a13bbe4de9b997b22797402025577798c3fcd8
Jürgen Geuter <tante@the-gay-bar.com>
2009-11-18 23:03:23 +0100

    Added smarter config file
    Now sections can be defined that allow flexible configurations.

--- gmaillabelpurge.py
+++ gmaillabelpurge.py
@@ -10,6 +10,8 @@ import imaplib
 import os.path
 import email.utils
 import datetime
+import string
+from ConfigParser import ConfigParser
 from optparse import OptionParser
 
 CONFIGFILE="~/.gmaillabelpurge"
@@ -23,51 +25,32 @@ def readConf():
     when the file isn't valid for some reason."""
     global _config
     _config={}
+    config=ConfigParser()
     try:
-        handle = open(os.path.expanduser(CONFIGFILE),"r")
-        data = handle.readlines()
+        data = config.read(os.path.expanduser(CONFIGFILE))
     except:
         raise SystemExit("""Please set up the configuration in %s
 Example:
 
 username=myuser@gmail.com
 password=MYPASSWORD
+[set1]
 maxage=30
 labels=LABEL1,LABEL2
+[set2]
+maxage=15
+labels=LABEL4,LABEL5
         
 """ % os.path.expanduser(CONFIGFILE))
-    if len(data)<2:
-        raise SystemExit("Please setup you configuration in %s!" % os.path.expanduser(CONFIGFILE))
-    for line in data:
-        param,value = line.split("=")
-        if param.strip().lower()=="username":
-            _config['username']=value.strip()
-        if param.strip().lower()=="password":
-            _config['password']=value.strip()
-        if param.strip().lower()=="maxage":
-            _config['maxage']=int(value.strip())
-        if param.strip().lower()=="labels":
-            labels=[]
-            for label in value.split(","):
-                labels.append(label.strip())
-            _config['labels']=labels
 
     try:
-        _config['username']
+        _config['username'] = config.get("DEFAULT","username")
     except:
         raise SystemExit("Please set a username in your config file")
     try:
-        _config['password']
+        _config['password'] = config.get("DEFAULT","password") 
     except:
         raise SystemExit("Please set a password in your config file")
-    try:
-        _config['maxage']
-    except:
-        raise SystemExit("Please set the maximum age of mails to keep (via 'maxage')")
-    try:
-        _config['labels']
-    except:
-        raise SystemExit("Please set labels to be purged")
 
     # now we set the gmail folder
     # German accounts call it [Google Mail], International ones [Gmail]
@@ -76,6 +59,20 @@ labels=LABEL1,LABEL2
     else:
         _config['folder'] = "Gmail"
 
+    _config['sections']=[]
+    for section in config.sections():
+        sectconf = {}
+        sectconf['name']   = section
+        try:
+            sectconf['labels'] = map(string.strip,config.get(section,"labels").split())
+        except:
+            raise SystemExit("No labels defined for section %s" % section)
+        try:
+            sectconf['maxage'] = config.get(section,"maxage")
+        except:
+            raise SystemExit("No maxage defined for section %s" % section)
+        _config['sections'].append(sectconf)
+    
 def purge(verbose=False,pretend=False):
     """Purge the labels given in the config file."""
     
@@ -94,69 +91,74 @@ def purge(verbose=False,pretend=False):
         print("Current time: %s" % now.isoformat())
         print("Will use the Foldername [%s]" % _config['folder'])
 
-    # go through the labels
-    for label in _config['labels']:
-        if verbose:
-            print("Checking label '%s'" % label)
-    
-        try:
-            status, count = server.select(label)
-        except:
-            raise SystemExit("The given label ('%s') doesn't seem to exist." % label)
-        
-        # get all messages
-        status, data = server.search(None, 'ALL')
-        
-        # get the UIDs so we can properly delete more than one
-        messages=[]
-        for message in data[0].split():
-            status, data = server.fetch(message, "UID")
-            for msg in data:
-                # we just want the real UID so we throw away the rest
-                messages.append(msg[msg.index("UID")+4:-1])
-        
-        # now go through messages
-        for message in messages:
+    # iterate over the sections
+    for section in _config['sections']:
+        # go through the labels
+        for label in section['labels']:
             if verbose:
-                print("Loading message %s" % message)
-            status, data = server.uid("fetch",message, "(UID BODY[HEADER.FIELDS (SUBJECT FROM DATE)])")
-            headers={}
-            for header in data[0][1].split("\n"):
-                try:
-                    name,value = header.strip().split(":",1)
-                    headers[name.strip().lower()] = value.strip()
-                except:
-                    # we just don't care what went wrong here
-                    pass
-        
-            datetuple = list(email.utils.parsedate(headers['date']))
-            # delete timezoneoffsets, might have to deal with that later
-            del datetuple[7]
-            del datetuple[7]
+                print("Checking label '%s'" % label)
+            try:
+                status, count = server.select(label)
+            except:
+                raise SystemExit("The given label ('%s') doesn't seem to exist." % label)
             
-            maildate = datetime.datetime(*datetuple)
-            delta=now-maildate
+            # get all messages
+            try:
+                status, data = server.search(None, 'ALL')
+            # might be too generic but gmail seems to allow select-ing unexisting labels
+            except:
+                raise SystemExit("The given label ('%s') doesn't seem to exist, there were at least problems with it." % label)
             
-            #check whether we wanna delete the mail
-            if delta.days>_config['maxage']:
-                if pretend:
-                    print("I would delete '%s' from '%s'" % (headers['subject'],headers['from'])) 
-
-                else:
-                    print("Deleting '%s' from '%s'" % (headers['subject'],headers['from'])) 
-                    try:
-                        #copy the mail to the trash
-                        server.uid("copy",message,"[%s]/Trash" % _config['folder'])
-                        #mark the original mail deleted
-                        typ, response = server.uid("store",message, '+FLAGS', r'(\Deleted)')
-                        #call expunge in order to really delete the messages marked
-                        server.expunge()
-                    except Exception, e:
-                        print("There was a problem deleting '%s' from '%s' (%s)" % (headers['subject'],headers['from'],repr(e)))        
+            # get the UIDs so we can properly delete more than one
+            messages=[]
+            for message in data[0].split():
+                status, data = server.fetch(message, "UID")
+                for msg in data:
+                    # we just want the real UID so we throw away the rest
+                    messages.append(msg[msg.index("UID")+4:-1])
             
-            else:
+            # now go through messages
+            for message in messages:
                 if verbose:
-                    print("Not Deleting '%s' from '%s'" % (headers['subject'],headers['from'])) 
+                    print("Loading message %s" % message)
+                status, data = server.uid("fetch",message, "(UID BODY[HEADER.FIELDS (SUBJECT FROM DATE)])")
+                headers={}
+                for header in data[0][1].split("\n"):
+                    try:
+                        name,value = header.strip().split(":",1)
+                        headers[name.strip().lower()] = value.strip()
+                    except:
+                        # we just don't care what went wrong here
+                        pass
+            
+                datetuple = list(email.utils.parsedate(headers['date']))
+                # delete timezoneoffsets, might have to deal with that later
+                del datetuple[7]
+                del datetuple[7]
+                
+                maildate = datetime.datetime(*datetuple)
+                delta=now-maildate
+             
+                #check whether we wanna delete the mail
+                if delta.days>section['maxage']:
+                    if pretend:
+                        print("I would delete '%s' from '%s'" % (headers['subject'],headers['from'])) 
+
+                    else:
+                        print("Deleting '%s' from '%s'" % (headers['subject'],headers['from'])) 
+                        try:
+                            #copy the mail to the trash
+                            server.uid("copy",message,"[%s]/Trash" % _config['folder'])
+                            #mark the original mail deleted
+                            typ, response = server.uid("store",message, '+FLAGS', r'(\Deleted)')
+                            #call expunge in order to really delete the messages marked
+                            server.expunge()
+                        except Exception, e:
+                            print("There was a problem deleting '%s' from '%s' (%s)" % (headers['subject'],headers['from'],repr(e)))        
+                
+                else:
+                    if verbose:
+                        print("Not Deleting '%s' from '%s'" % (headers['subject'],headers['from'])) 
         
     # close the connection to the server
     server.close()
